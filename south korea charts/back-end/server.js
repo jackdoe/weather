91c86367dea62fc992edf4e695ea071a3e5017e3
Husam Lahm <37015049+ThreePalmTrees@users.noreply.github.com>
2018-10-18 02:28:26 +0200

    code refactoring

--- south korea charts/back-end/server.js
+++ south korea charts/back-end/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const requireDir = require("require-dir");
 
-const stats = requireDir("./sources/stats");
-const statsArr = Object.values(stats);
-
 const app = express();
-const port = 3001;
 
 app.get("/api/stats", (req, res) => {
+  const stats = requireDir("./sources/stats");
+  const statsArr = Object.values(stats);
   res.json(statsArr);
 });
+
+const port = 3001;
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
