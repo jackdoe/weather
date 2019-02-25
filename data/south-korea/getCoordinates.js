f5169c816d9223ab665422915af98a8547a98d1f
Husam Lahm <37015049+ThreePalmTrees@users.noreply.github.com>
2019-02-25 10:51:35 +0100

    Update getCoordinates.js

--- data/south-korea/getCoordinates.js
+++ data/south-korea/getCoordinates.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { sleep } = require("sleep");
 
 async function getCoordinates(cityName) {
-  const KEY = "3qrtTDxHwEEnR3z32ztw1tSZHVBcnK5m";
+  const KEY = "3qrtTDxHwEEnR3z32ztw1tSZHVBcnK5M";
   const API_URL = `http://www.mapquestapi.com/geocoding/v1/address?key=${KEY}&location=${cityName},KOR`;
   return axios
     .get(API_URL)
